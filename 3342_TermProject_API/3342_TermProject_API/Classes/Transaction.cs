a5b7bc9ffc402e3c32e184330760fd9a9c7abc6d
unknown <16grodgers@gmail.com>
2019-11-10 09:50:45 -0500

    All Project Required Methods and Classses

---
+++ 3342_TermProject_API/3342_TermProject_API/Classes/Transaction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _3342_TermProject_API.Classes
+{
+    public class Transaction
+    {
+    }
+}
---
+++ 3342_TermProject_API/3342_TermProject_API/Controllers/MerchantTransactionController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using _3342_TermProject_API.Classes;
+
+namespace _3342_TermProject_API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/service/PaymentGateway")]
+    [ApiController]
+    public class MerchantTransactionController : ControllerBase
+    {
+        [HttpPost]
+        public Wallet CreateVirtualWallet(Wallet AccountHolderInformation, Wallet MerchantAccountID, Wallet APIKey)
+        {
+            Wallet wallet = new Wallet();
+            return wallet ;
+        }
+        // GET: api/MerchantTransaction
+        [HttpGet]
+        public List<Wallet> GetTransactions()
+        {
+            List<Wallet> wallets = new List<Wallet>();
+            return wallets;
+        }
+        [HttpPost]
+        public Wallet ProcessPayment(Wallet VirtualWalletID, Transaction Amount, Transaction type, Merchant MerchantAccountID, Wallet APIKey )
+        {
+            Wallet wallet = new Wallet();
+            return wallet;
+        }
+        [HttpPut]
+        public Transaction UpdatePaymentAccount()
+        {
+            Transaction transaction = new Transaction();
+            return transaction;
+        }
+        [HttpPut]
+        public Transaction FundAccount(Wallet VirtualWalletID, Transaction Amount, Merchant MerchantAccountID, Wallet APIKey)
+        {
+            Transaction transaction = new Transaction();
+            return transaction;
+        }     
+    }
+}
